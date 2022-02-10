4ae7b03478f236927cbd125a12b27fb946de118c
Mercybett <merjep551@gmail.com>
2022-02-10 21:54:50 +0300

    add dynamic sensei route and implement delete logic

---
+++ pages/api/sensei/[id].js
@@ -0,0 +1,17 @@
+import { prisma } from '../../../lib/prisma'
+
+export default async function handler(req, res) {
+  const { id } = req.params
+  if (req.method === 'DELETE') {
+    try {
+      const sensei = await prisma.sensei.delete({
+        where: {
+          id: Number(id),
+        },
+      })
+      res.json(sensei)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
