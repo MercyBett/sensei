3fc6b6f3254ab401afd9bad9f583b3fad65f267c
Mercybett <merjep551@gmail.com>
2022-02-10 14:38:17 +0300

    change card focus&hover text color

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-      <div className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600">
+      <div className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-purple-600 focus:text-purple-600">
         <h3 className="text-2xl font-bold">Topic</h3>
         <p className="mt-4 text-xl">Sensei</p>
       </div>
