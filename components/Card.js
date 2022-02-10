f378d9f312112f0503bb641aefc7979c968cd707
Mercybett <merjep551@gmail.com>
2022-02-10 21:45:25 +0300

    add update annd delete buttons

--- components/Card.js
+++ components/Card.js
@@ -9,6 +9,24 @@ function Card({ senseis }) {
             <div className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-purple-600 focus:text-purple-600">
               <h3 className="text-2xl font-bold">{sensei.topic}</h3>
               <p className="mt-4 text-xl">{sensei.sensei}</p>
+              <button
+                onClick={() =>
+                  setForm({
+                    title: sensei.title,
+                    content: sensei.content,
+                    id: sensei.id,
+                  })
+                }
+                className="mr-3 rounded bg-blue-500 px-3 text-white"
+              >
+                Update
+              </button>
+              <button
+                onClick={() => deleteNote(sensei.id)}
+                className="rounded bg-red-500 px-3 text-white"
+              >
+                Delete
+              </button>
             </div>
           </li>
         ))}
