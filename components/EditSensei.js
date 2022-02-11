564f8403eee0ac320514a7e6a26fd926c159ced6
Mercybett <merjep551@gmail.com>
2022-02-11 15:57:45 +0300

    replace form with data

--- components/EditSensei.js
+++ components/EditSensei.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 function EditSensei({ form, closeModal }) {
-  const [form, setForm] = useState({ topic: form.topic, sensei: form.sensei })
+  const [data, setData] = React.useState()
   const id = form.id
+
+  async function handleSubmit(data) {
+    await fetch(`api/sensei/${id}`, {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'PUT',
+      body: JSON.stringify(form),
+    })
+  }
 
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault()
-          handleSubmit(form)
+          handleSubmit(data)
         }}
         className="mx-auto flex w-auto min-w-[25%] max-w-min flex-col items-stretch space-y-6"
       >
@@ -17,13 +25,13 @@ function EditSensei({ form, closeModal }) {
           type="text"
           placeholder="Topic e.g Next.js"
           value={form.topic}
-          onChange={(e) => setForm({ ...form, topic: e.target.value })}
+          onChange={(e) => setData({ ...data, topic: e.target.value })}
           className="rounded border-2 border-gray-600 p-1"
         />
         <textarea
           placeholder="Sensei e.g Lee Robinson"
           value={form.sensei}
-          onChange={(e) => setForm({ ...form, sensei: e.target.value })}
+          onChange={(e) => setData({ ...data, sensei: e.target.value })}
           className="rounded border-2 border-gray-600 p-1"
         />
         <button type="submit" className="rounded bg-purple-500 p-1 text-white">
