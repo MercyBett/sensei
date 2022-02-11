8ffed2b3877f3d22e5729ecfd1649456accb713c
Mercybett <merjep551@gmail.com>
2022-02-11 21:28:01 +0300

    update create endpoint to capture the author

--- pages/api/create.js
+++ pages/api/create.js
@@ -1,11 +1,15 @@
 import { prisma } from '../../lib/prisma'
+import { getSession } from 'next-auth/react'
 export default async function handler(req, res) {
   const { topic, sensei } = req.body
+  const session = await getSession({ req })
+
   try {
     await prisma.sensei.create({
       data: {
         topic,
         sensei,
+        author: { connect: { email: session?.user?.email } },
       },
     })
     res.status(200).json({ message: 'new sensei created successfully' })
